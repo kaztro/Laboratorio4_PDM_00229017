37997b4f31f6569f421b173c5ef67f4339fd81e9
kaztro <00229017@uca.edu.sv>
2019-04-05 15:33:07 -0600

    Terminado PAPUHgit add .git add .git add .git add .!

--- app/src/main/java/com/example/laboratorio4_pdm_00229017/activities/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio4_pdm_00229017/activities/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.laboratorio4_pdm_00229017.activities
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import com.example.laboratorio4_pdm_00229017.R
 import com.example.laboratorio4_pdm_00229017.adapters.MovieAdapter
+import com.example.laboratorio4_pdm_00229017.network.NetworkUtils
 import com.example.laboratorio4_pdm_00229017.pojos.Movie
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var movieAdapter: MovieAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
 
-    private var movieList: ArrayList<Movie> = ArrayList<>
+    private var movieList: ArrayList<Movie> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,10 +50,42 @@ class MainActivity : AppCompatActivity() {
             FetchMovie().execute(movie_name_et.text.toString())
         }
     }
-
     fun addMovieToList(movie: Movie) {
         movieList.add(movie)
+        movieAdapter.changeList(movieList)
+        Log.d("Number", movieList.size.toString())
     }
 
+    fun movieItemClicked(item: Movie) {}
+
+    private inner class FetchMovie: AsyncTask<String, Void, String>() {
+
+    override fun doInBackground(vararg params: String?): String {
+
+        if(params.isNullOrEmpty()) return ""
+
+        val movieName = params[0]
+
+        val movieUrl = NetworkUtils().buildSearchUrl(movieName!!)
+
+        return try { NetworkUtils().getResponseFromHttpUrl(movieUrl) }
+        catch (e: IOException) { "" }
+    }
+
+    override fun onPostExecute(movieInfo: String?) {
+        super.onPostExecute(movieInfo)
+
+        if(!movieInfo!!.isEmpty()) {
+            val movieJson = JSONObject(movieInfo)
+            if(movieJson.getString("Response") == "True") {
+                val movie = Gson().fromJson<Movie>(movieInfo, Movie::class.java)
+
+                addMovieToList(movie)
+            } else {
+                Snackbar.make(main_ll, "No existe la pelicula en la base", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
 
 }
